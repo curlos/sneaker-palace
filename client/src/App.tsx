30dfee5e0f57a25eac03671855e1540c145031cf
Carlos Martinez <curlosmart@gmail.com>
2021-11-01 09:16:56 -0400

    Added routing to access different pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,51 +1,42 @@
-import axios from 'axios'
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import ProductList from "./pages/ProductList";
+import Home from './pages/Home'
+import FullShoePage from './pages/FullShoePage'
 
 const App = () => {
+
+  
   return (
-    <div className="p-4 flex justify-between text-base">
-      <div className="inline-flex items-center text-base text-gray-700">Showing 1 to 10 of 97 results</div>
+    <Router>
 
-      <div className="flex">
-        <a className="inline-flex items-center text-gray-600 border border-r-0 border-gray-300 rounded-l-lg px-3 py-2 hover:bg-gray-50 cursor-pointer">
-          <ChevronLeftIcon className="h-5 w-5"/>
-        </a>
+      <div className="m-0 box-border">
+        
 
-        <a className="z-10 inline-flex items-center bg-indigo-50 border border-indigo-500 text-indigo-600 px-4 py-2 hover:bg-indigo-100 cursor-pointer">
-          1
-        </a>
+        <Switch>
+          <Route path="/shoes">
+            <ProductList />
+          </Route>
 
-        <a className="z-10 inline-flex items-center border border-r-0 border-gray-300 text-gray-500 px-4 py-2 hover:bg-gray-50 cursor-pointer">
-          2
-        </a>
+          <Route path="/shoe/:shoeID">
+            <FullShoePage />
+          </Route>
 
-        <a className="z-10 inline-flex items-center border border-r-0 border-gray-300 text-gray-500 px-4 py-2 hover:bg-gray-50 cursor-pointer">
-          3
-        </a>
 
-        <a className="z-10 inline-flex items-center border border-r-0 border-gray-300 text-gray-500 px-4 py-2 hover:bg-gray-50 cursor-pointer">
-          ...
-        </a>
+          <Route path="/">
+            <Home />
+          </Route>
 
-        <a className="z-10 inline-flex items-center border border-r-0 border-gray-300 text-gray-500 px-4 py-2 hover:bg-gray-50 cursor-pointer">
-          8
-        </a>
-
-        <a className="z-10 inline-flex items-center border border-r-0 border-gray-300 text-gray-500 px-4 py-2 hover:bg-gray-50 cursor-pointer">
-          9
-        </a>
-
-        <a className="z-10 inline-flex items-center border border-r-0 border-gray-300 text-gray-500 px-4 py-2 hover:bg-gray-50 cursor-pointer">
-          10
-        </a>
-
-        <a className="inline-flex items-center text-gray-600 border border-gray-300 rounded-r-lg px-3 py-2 hover:bg-gray-50">
-          <ChevronRightIcon className="h-5 w-5"/>
-        </a>
+        </Switch>
       </div>
 
-      
-    </div>
+    
+    </Router>
 
   );
 }
