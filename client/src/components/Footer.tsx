df386b1f37fe77dfd35a73381033d5ed4538c7e2
Carlos Martinez <curlosmart@gmail.com>
2021-11-02 16:06:45 -0400

    footer added

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const Footer = () => {
+  return (
+    <div className="bg-black text-white text-xs mt-5 pt-10">
+      <div className="flex justify-center py-5 px-10 border-0 border-b border-solid border-gray-400">
+        <div className="flex-2">
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-sm">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+        </div>
+
+        <div className="flex-2">
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-sm">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+        </div>
+
+        <div className="flex-2">
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-sm">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+        </div>
+
+        <div className="flex-2">
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-sm">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+        </div>
+
+        <div className="flex-2">
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-sm">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+        </div>
+
+        <div className="flex-2">
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-sm">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+          <div><Link to="/shoe/c8e2e33f-4272-4e3b-8a2c-a4a650bc9c0d" className="text-gray-400">Air Jordan</Link></div>
+        </div>
+
+      </div>
+
+      <div className="px-10 py-4 flex justify-between items-center">
+        <div className="flex gap-4 text-gray-400">
+          <div className="cursor-pointer">Terms</div>
+          <div className="cursor-pointer">Privacy</div>
+          <div className="cursor-pointer">Press</div>
+          <div className="cursor-pointer">Jobs</div>
+        </div>
+
+        <div className="text-gray-400">&#169; Shoe Shop Miami LLC. All Rights Reserved</div>
+
+        <div className="text-2xl">
+          <i className="fab fa-twitter-square mr-3 cursor-pointer"></i>
+          <i className="fab fa-facebook-square mr-3 cursor-pointer"></i>
+          <i className="fab fa-instagram-square mr-3 cursor-pointer"></i>
+          <i className="fab fa-youtube-square mr-3 cursor-pointer"></i>
+        </div>
+        
+      </div>
+    </div>
+  )
+}
