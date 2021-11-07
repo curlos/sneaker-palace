154cb9bac77371cedcccfac890e386810d1c9975
Carlos Martinez <curlosmart@gmail.com>
2021-11-06 22:32:33 -0400

    sidebar filters added

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,48 +1,146 @@
-import { ChevronUpIcon } from "@heroicons/react/solid";
+import React, { useState } from 'react';
+import { ChevronUpIcon, ChevronDownIcon, CheckIcon } from "@heroicons/react/solid";
 
-const Sidebar = () => {
+interface Props {
+  filters: {
+    colors: any,
+    brands: any,
+    genders: any,
+    priceRanges: any,
+    shoeSizes: any
+  },
+  setFilters: any
+}
+
+const Sidebar = ({ filters, setFilters }: Props) => {
+
+  console.log(filters)
+  
+  const [showColors, setShowColors] = useState(false)
+  const [showBrands, setShowBrands] = useState(false)
+  const [showGender, setShowGender] = useState(false)
+  const [showPriceRanges, setShowPriceRanges] = useState(false)
+  const [showSizes, setShowSizes] = useState(false)
+
+  const handleColorClick = (color: string) => {
+    setFilters({...filters, colors: {...filters.colors, [color]: !filters.colors[color] }})
+  }
+
+  const handleBrandClick = (brand: string) => {
+    setFilters({...filters, brands: {...filters.brands, [brand]: !filters.brands[brand] }})
+  }
+
+  const handleGenderClick = (gender: string) => {
+    setFilters({...filters, genders: {...filters.genders, [gender]: !filters.genders[gender] }})
+  }
+
+  const handlePriceClick = (priceRange: string) => {
+    setFilters({...filters, priceRanges: {...filters.priceRanges, [priceRange]: {...filters.priceRanges[priceRange], checked: !filters.priceRanges[priceRange].checked} }})
+  }
+
+  const handleSizeClick = (size: string) => {
+    setFilters({...filters, shoeSizes: {...filters.shoeSizes, [size]: !filters.shoeSizes[size] }})
+  }
 
   return (
-    <aside className="h-screen sticky top-0 overflow-y-auto p-5 w-full flex-2">
+    <aside className="top-0 p-5 w-full flex-2 flex-grow-1 overflow-y-auto bg-white">
 
-      <div className="flex justify-between items-center bg-white">
-        <span>Color</span> <ChevronUpIcon className="h-6 w-6"/>
+      <div className="border-0 border-b border-solid border-gray-300 py-3">
+        
+        <div className="flex justify-between items-center cursor-pointer" onClick={() => setShowColors(!showColors)}>
+          <div className="font-bold mb-3">Color</div> {showColors ? <ChevronUpIcon className="h-6 w-6"/> : <ChevronDownIcon className="h-6 w-6"/>}
+        </div>
+        {showColors ? (
+          <div className="flex flex-wrap justify-center gap-5 p-3">
+            {Object.keys(filters.colors).map((color) => {
+              return (
+                <div className="flex flex-col items-center h-13 w-13">
+                  <div className={`h-7 w-7 rounded-full bg-${color === 'black' || color === 'white' ? color : (color + '-500')} ${color === 'white' ? 'border border-gray-300' : ''} ${color === 'white' ? 'text-black' : 'text-white'}`} onClick={() => handleColorClick(color)}>
+                    {filters.colors[color] ? <CheckIcon /> : null}
+                  </div>
+                  <div className="capitalize">{color}</div>
+                </div>
+              )
+            })} 
+          </div>
+        ) : null}
+        
       </div>
 
-      <div className="flex justify-between items-center">
-        <span>Brand</span> <ChevronUpIcon className="h-6 w-6"/>
+      <div className="border-0 border-b border-solid border-gray-300 py-3">
+        <div className="flex justify-between items-center cursor-pointer" onClick={() => setShowBrands(!showBrands)}>
+          <span className="font-bold mb-3">Brand</span> {showBrands ? <ChevronUpIcon className="h-6 w-6"/> : <ChevronDownIcon className="h-6 w-6"/>}
+        </div>
+
+        {showBrands ? (
+          <div>
+            {Object.keys(filters.brands).map((brand) => {
+              return (
+                <label className="flex items-center">
+                  <input type="checkbox" className="mr-2 cursor-pointer" checked={filters.brands[brand]} onClick={() => handleBrandClick(brand)}></input>
+                  <span>{brand}</span>
+                </label>
+                
+              )
+            })}
+          </div>
+        ) : null}
       </div>
 
-      <div className="flex justify-between items-center">
-        <span>Gender</span> <ChevronUpIcon className="h-6 w-6"/>
+      <div className="border-0 border-b border-solid border-gray-300 py-3">
+        <div className="flex justify-between items-center cursor-pointer" onClick={() => setShowGender(!showGender)}>
+          <span className="font-bold mb-3">Gender</span> {showGender ? <ChevronUpIcon className="h-6 w-6"/> : <ChevronDownIcon className="h-6 w-6"/>}
+        </div>
+
+        {showGender ? (
+          <div>
+            {Object.keys(filters.genders).map((gender) => {
+              return (
+                <label className="flex items-center">
+                  <input type="checkbox" className="mr-2 cursor-pointer" checked={filters.genders[gender]} onClick={() => handleGenderClick(gender)}></input>
+                  <span>{gender}</span>
+                </label>
+                
+              )
+            })}
+          </div>
+        ) : null}
       </div>
 
-      <div className="flex justify-between items-center">
-        <span>Shop by Price</span> <ChevronUpIcon className="h-6 w-6"/>
+      <div className="border-0 border-b border-solid border-gray-300 py-3">
+        <div className="flex justify-between items-center cursor-pointer" onClick={() => setShowPriceRanges(!showPriceRanges)}>
+          <span className="font-bold mb-3">Shop by Price</span> {showPriceRanges ? <ChevronUpIcon className="h-6 w-6"/> : <ChevronDownIcon className="h-6 w-6"/>}
+        </div>
+
+        {showPriceRanges ? (
+          <div>
+            {Object.keys(filters.priceRanges).map((priceRange) => {
+              return (
+                <label className="flex items-center">
+                  <input type="checkbox" className="mr-2 cursor-pointer" checked={filters.priceRanges[priceRange].checked} onClick={() => handlePriceClick(priceRange)}></input>
+                  <span>{priceRange}</span>
+                </label>
+                
+              )
+            })}
+          </div>
+        ) : null}
       </div>
 
-      <div className="flex justify-between items-center">
-        <span>Shop by Price</span> <ChevronUpIcon className="h-6 w-6"/>
-      </div>
+      <div className="py-3">
+        <div className="flex justify-between items-center cursor-pointer" onClick={() => setShowSizes(!showSizes)}>
+          <span className="font-bold mb-3">Size</span> {showSizes ? <ChevronUpIcon className="h-6 w-6"/> : <ChevronDownIcon className="h-6 w-6"/>}
+        </div>
 
-      <div className="flex justify-between items-center">
-        <span>Shop by Price</span> <ChevronUpIcon className="h-6 w-6"/>
-      </div>
-
-      <div className="flex justify-between items-center">
-        <span>Shop by Price</span> <ChevronUpIcon className="h-6 w-6"/>
-      </div>
-
-      <div className="flex justify-between items-center">
-        <span>Shop by Price</span> <ChevronUpIcon className="h-6 w-6"/>
-      </div>
-
-      <div className="flex justify-between items-center">
-        <span>Shop by Price</span> <ChevronUpIcon className="h-6 w-6"/>
-      </div>
-      
-      <div className="flex justify-between items-center">
-        <span>Shop by Price</span> <ChevronUpIcon className="h-6 w-6"/>
+        {showSizes ? (
+          <div className="flex flex-wrap">
+            {Object.keys(filters.shoeSizes).map((shoeSize) => {
+              return (
+                <div className={`h-10 w-12 inline-flex items-center justify-center ${filters.shoeSizes[shoeSize] ? ' border-2 border-black' : 'border border-gray-300'} m-1 rounded-lg cursor-pointer`} onClick={() => handleSizeClick(shoeSize)}>{shoeSize}</div>
+              )
+            })}
+          </div>
+        ) : null}
       </div>
     </aside>
   )
--- client/src/pages/ProductList.tsx
+++ client/src/pages/ProductList.tsx
@@ -4,10 +4,102 @@ import axios from 'axios'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
 import SmallShoe from '../components/SmallShoe'
+import { Shoe } from '../types/types'
 
 const ProductList = () => {
 
+  const SHOE_SIZES = ['4', '4.5', '5', '5.5', '6', '6.5', '7', '7.5', '8', '8.5', '9', '9.5', '10', '10.5', '11', '11.5', '12', '12.5', '13', '13.5', '14', '14.5', '15', '16', '17' ]
+
   const [shoes, setShoes] = useState([])
+  const [filters, setFilters] = useState({
+    colors: {
+      'red': false,
+      'white': false,
+      'yellow': false,
+      'black': false,
+      'blue': false,
+      'brown': false,
+      'green': false,
+      'gray': false,
+      'pink': false,
+      'purple': false,
+    },
+    brands: {
+      'Adidas': false,
+      'Air Jordan': false,
+      'Alexander McQueen': false,
+      "Asics": false,
+      "Balenciaga": false,
+      "Burberry": false,
+      "Chanel": false,
+      "Common Projects": false,
+      "Converse": false,
+      "Crocs": false,
+      "Diadora": false,
+      "Dior": false,
+      "Gucci": false,
+      "Jordan": false,
+      "Li Ning": false,
+      "New Balance": false,
+      "Nike": false,
+      "Louis Vuitton": false,
+      "Off-White": false,
+      "Prada": false,
+      "Puma": false,
+      "Reebok": false,
+      "Saint Laurent": false,
+      "Saucony": false,
+      "Vans": false,
+      "Yeezy": false,
+    },
+    genders: {
+      "Men": false,
+      "Women": false,
+      "Unisex": false
+    },
+    priceRanges: {
+      "$0 - $25": {
+        priceRanges: {
+          low: 0,
+          high: 25
+        },
+        checked: false
+      },
+      "$25 - $50": {
+        priceRanges: {
+          low: 25,
+          high: 50
+        },
+        checked: false
+      },
+      "$50 - $100": {
+        priceRanges: {
+          low: 50,
+          high: 100
+        },
+        checked: false
+      },
+      "$100 - $150": {
+        priceRanges: {
+          low: 100,
+          high: 150
+        },
+        checked: false
+      },
+      "$150+": {
+        priceRanges: {
+          low: 150,
+          high: null
+        },
+        checked: false
+        
+      }
+
+    },
+    shoeSizes: {
+      ...SHOE_SIZES.reduce((a, v) => ({ ...a, [v]: false}), {}) 
+    }
+  })
 
   useEffect(() => {
     const fetchFromAPI = async () => {
@@ -22,14 +114,13 @@ const ProductList = () => {
 
   return (
     <div className="text-xl-lg">
-      <Navbar />
       <div className="flex">
-        <Sidebar />
+        <Sidebar filters={filters} setFilters={setFilters}/>
 
-        <div className="flex justify-center flex-wrap flex-8 bg-gray-100">
-          {shoes.map((shoe) => {
+        <div className="flex justify-center flex-wrap flex-10">
+          {shoes.map((shoe: Shoe) => {
             return (
-              <SmallShoe shoe={shoe} />
+              <SmallShoe key={shoe.shoeID} shoe={shoe} />
             )
           })}
         </div>
