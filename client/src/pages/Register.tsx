4b5eb8ea792ecf0db1c4c538da6c9dcefc83080c
Carlos Martinez <curlosmart@gmail.com>
2021-11-07 08:32:51 -0500

    register functionality added

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,17 +1,44 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
+import axios from 'axios'
 
 const Register = () => {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const history = useHistory()
+  
+  const handleRegisterUser = async (e: React.FormEvent) => {
+    e.preventDefault()
+
+    const body = {
+      email,
+      password,
+      firstName,
+      lastName,
+    }
+    console.log(body)
+    try {
+      const response = await axios.post(`http://localhost:8888/auth/register`, body)
+      console.log(response.data)
+      history.push('/')
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
-    <form className="h-screen bg-login-image bg-cover flex justify-center items-start">
+    <form className="h-screen bg-login-image bg-cover flex justify-center items-start" onSubmit={handleRegisterUser}>
       <div className="flex flex-col gap-4 items-center bg-white w-2/5 p-4 rounded-lg my-6">
         <span className="font-bold text-2xl">BECOME A MEMBER</span>
-        <input type="email" placeholder="Email address" className="border border-gray-300 rounded-lg p-2 px-4 w-full focus:outline-none"></input>
-        <input type="password" placeholder="Password" className="border border-gray-300 rounded-lg p-2 px-4 w-full focus:outline-none"></input>
-        <input type="text" placeholder="First Name" className="border border-gray-300 rounded-lg p-2 px-4 w-full focus:outline-none"></input>
-        <input type="text" placeholder="Last Name" className="border border-gray-300 rounded-lg p-2 px-4 w-full focus:outline-none"></input>
+        <input type="email" placeholder="Email address" value={email} onChange={(e) => setEmail(e.target.value)} className="border border-gray-300 rounded-lg p-2 px-4 w-full focus:outline-none"></input>
+        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} className="border border-gray-300 rounded-lg p-2 px-4 w-full focus:outline-none"></input>
+        <input type="text" placeholder="First Name" value={firstName} onChange={(e) => setFirstName(e.target.value)} className="border border-gray-300 rounded-lg p-2 px-4 w-full focus:outline-none"></input>
+        <input type="text" placeholder="Last Name" value={lastName} onChange={(e) => setLastName(e.target.value)} className="border border-gray-300 rounded-lg p-2 px-4 w-full focus:outline-none"></input>
 
-        <button className="bg-black text-white w-full py-2 rounded-lg hover:bg-gray-600">SIGN UP</button>
+        <button className="bg-black text-white w-full py-2 rounded-lg hover:bg-gray-600" onClick={handleRegisterUser}>SIGN UP</button>
         <span className="text-gray-500">Already a member? <Link to="/login" className="text-black underline">Log in.</Link></span>
       </div>
     </form>
