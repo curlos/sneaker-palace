3aac64a19ff0a70ed68b0be6f8678f87338448d2
Carlos Martinez <curlosmart@gmail.com>
2021-11-06 22:32:09 -0400

    user, store and cart redux added

---
+++ client/src/redux/cartRedux.jsx
@@ -0,0 +1,21 @@
+import { CaseReducer, createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { Observable } from 'rxjs'
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    products: [],
+    quantity: 0,
+    total: 0,
+  },
+  reducers: {
+    addProduct: (state, action) => {
+      state.quantity += 1
+      state.products.push(action.payload)
+      state.total += action.payload.price * action.payload.quantity
+    }
+  }
+})
+
+export const { addProduct } = cartSlice.actions
+export default cartSlice.reducer
