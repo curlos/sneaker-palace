b0cf786b810b44d08d82db160f9574a9e2727bb2
Carlos Martinez <curlosmart@gmail.com>
2021-11-07 19:18:14 -0500

    add shoes and favorite (from client)

--- client/src/pages/FullShoePage.tsx
+++ client/src/pages/FullShoePage.tsx
@@ -1,9 +1,16 @@
-import axios from 'axios'
 import React, {useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import { useSelector, useDispatch } from "react-redux";
+import { CartState, IProduct, UserType } from "../types/types";
+import { RootState } from "../redux/store";
+import axios from 'axios'
 import moment from 'moment'
-import { HeartIcon } from '@heroicons/react/outline'
+import { HeartIcon as HeartSolid } from '@heroicons/react/solid'
+import { HeartIcon as HeartOutline } from '@heroicons/react/outline'
 import { Shoe } from '../types/types'
+import { updateUser } from '../redux/userRedux'
+import { updateCart } from '../redux/cartRedux';
+import { current } from '@reduxjs/toolkit';
 
 const SHOE_SIZES = ['4', '4.5', '5', '5.5', '6', '6.5', '7', '7.5', '8', '8.5', '9', '9.5', '10', '10.5', '11', '11.5', '12', '12.5', '13', '13.5', '14', '14.5', '15', '16', '17' ]
 
@@ -12,7 +19,12 @@ const AVERAGE_WOMEN_FOOT_SIZE = '8.5'
 
 const FullShoePage = () => {
 
+  const user: Partial<UserType> = useSelector((state: RootState) => state.user.currentUser)
+  const cart: Partial<CartState> = useSelector((state: RootState) => state.cart.currentCart)
+  const currentCart = cart?.currentCart
+
   const { shoeID }: { shoeID: string } = useParams()
+  const dispatch = useDispatch()
   
   const [shoe, setShoe] = useState<Partial<Shoe>>({})
   const [selectedSize, setSelectedSize] = useState(AVERAGE_MAN_FOOT_SIZE)
@@ -28,6 +40,41 @@ const FullShoePage = () => {
     }
     fetchFromAPI()
   }, [])
+
+  const handleAddToCart = async () => {
+    if (currentCart && currentCart.products && shoe && shoe.shoeID) {
+      console.log('adding to cart')
+      const newProduct: Partial<IProduct> = {
+        productID: shoe.shoeID,
+        size: Number(selectedSize),
+        quantity: 1,
+      }
+      const currentProducts = currentCart?.products
+
+      const body = { products: [...currentProducts, newProduct]}
+      console.log(body)
+
+      const response = await axios.put(`http://localhost:8888/cart/${currentCart?._id}`, body)
+      const newCart = response.data
+      console.log(newCart)
+      dispatch(updateCart({currentCart: newCart}))
+    }
+  }
+
+  const handleFavorite = async () => {
+    if (!user) {
+      return
+    }
+
+    const body = {
+      shoeID: shoeID,
+      userID: user['_id']
+    }
+    const response = await axios.put('http://localhost:8888/shoes/favorite', body)
+    console.log(response.data)
+    dispatch(updateUser(response.data.updatedUser))
+    setShoe(response.data.updatedShoe)
+  }
 
   console.log(shoe)
 
@@ -56,13 +103,15 @@ const FullShoePage = () => {
             </div>
 
             <div className="flex justify-center flex-wrap">
-              <button className="bg-black text-white rounded-full flex-2 py-3 mr-5 my-5 hover:bg-gray-700">
+              <button className="bg-black text-white rounded-full flex-2 py-3 mr-5 my-5 hover:bg-gray-700" onClick={handleAddToCart}>
                 Add to Bag
               </button>
 
-              <button className="flex justify-center items-center bg-white border border-gray-300 text-black rounded-full flex-2 my-5 hover:border-gray-600">
-                Favorite <HeartIcon className="h-5 w-5 mx-3"/>
+              <button className="flex justify-center items-center bg-white border border-gray-300 text-black rounded-full flex-2 my-5 hover:border-gray-600" onClick={handleFavorite}>
+                {user && shoe?._id && user?.favorites?.includes(shoe?._id) ? <span className="inline-flex items-center"> <HeartSolid className="mr-2 h-5 w-5" /></span> : <span className="inline-flex items-center"> <HeartOutline className="mr-2 h-5 w-5" /></span>}
+                {shoe?.favorites?.length}
               </button>
+
             </div>
 
             <div className="my-5">
@@ -70,11 +119,11 @@ const FullShoePage = () => {
             </div>
 
             <div>
-              <span className="font-bold">Colorway:</span> {shoe.colorway}
+              <span className="font-bold">Colorway:</span> {shoe?.colorway}
             </div>
 
             <div>
-              <span className="font-bold">Gender:</span> {shoe.gender?.toUpperCase()}
+              <span className="font-bold">Gender:</span> {shoe?.gender?.toUpperCase()}
             </div>
 
             <div>
