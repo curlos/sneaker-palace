2184d2158c52f58c3fadb97e2f0ac5d3f98f7ac7
Carlos Martinez <curlosmart@gmail.com>
2021-11-08 17:13:22 -0500

    small review comp. added for profile page

---
+++ client/src/components/SmallReview.tsx
@@ -0,0 +1,82 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { IRating, Shoe, UserType } from '../types/types'
+import StarRatingComponent from 'react-star-rating-component'
+import { Link } from 'react-router-dom'
+import moment from 'moment'
+
+interface Props {
+  review: IRating,
+  author: Partial<UserType>
+}
+
+const SmallReview = ({ review, author }: Props) => {
+
+  const [shoe, setShoe] = useState<Partial<Shoe>>({})
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+    setLoading(true)
+
+    const fetchFromAPI = async () => {
+      console.log(review.shoeID)
+      const response = await axios.get(`http://localhost:8888/shoes/${review.shoeID}`)
+      setShoe(response.data)
+      setLoading(false)
+    }
+    fetchFromAPI()
+  }, [])
+  
+
+  return (
+    <div className="border border-gray-300 rounded-lg bg-white mb-4">
+      <div className="flex items-center gap-3 text-sm my-3 border-0 border-b border-solid border-gray-300 p-3">
+        <div>
+          <img src="https://images-na.ssl-images-amazon.com/images/S/amazon-avatars-global/default._CR0,0,1024,1024_SX460_.png" alt={author?.firstName} className="h-7 w-7"/>
+        </div>
+        <div>{author.firstName} {author.lastName}</div>
+        <div className="text-gray-600 text-xs">reviewed a shoe • {moment(review.createdAt).format('MMMM Do, YYYY')}</div>
+      </div>
+      <div className="p-3">
+        <div className="flex items-center gap-2">
+          <StarRatingComponent
+                name={'Rating'}
+                value={review.ratingNum}
+                starCount={5}
+                editing={false}
+                starColor={'#10B981'}
+          />
+          <div className="text-sm text-orange-700">Verified Purchase</div>
+        </div>
+        <div className="font-bold">{review.summary}</div>
+        <div className="text-sm">{review.text}</div>
+        <div className="border border-gray-300 rounded-lg bg-white mt-2">
+          <Link to={`/shoe/${shoe.shoeID}`}>
+            <div className="flex items-center gap-2">
+              <div>
+                <img src={shoe?.image?.original} alt={shoe.name} className="h-24 w-24"/>
+              </div>
+              <div>
+                <div className="text-sm">{shoe.name}</div>
+                <div className="flex items-center gap-2">
+                  <StarRatingComponent
+                        name={'Rating'}
+                        value={4}
+                        starCount={5}
+                        editing={false}
+                        starColor={'#10B981'}
+                  />
+                  <div className="text-sm text-gray-700">{shoe?.ratings?.length}</div>
+                </div>
+              </div>
+            </div>
+          </Link>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default SmallReview
