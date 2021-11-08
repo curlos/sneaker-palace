59ebac964aa5027095163a4e1e8e5783e4266718
Carlos Martinez <curlosmart@gmail.com>
2021-11-07 19:16:43 -0500

    cart router added

---
+++ server/routes/cartRouter.ts
@@ -0,0 +1,66 @@
+import { Request, Response } from 'express'
+
+const User = require('../models/User')
+const Shoe = require('../models/Shoe')
+const Cart = require('../models/Cart')
+
+const router = require('express').Router()
+
+// Create cart
+
+router.post('/:userID', async (req: Request, res: Response) => {
+  const newCart = new Cart({
+    userID: req.params.userID,
+    products: []
+  })
+
+  const savedCart = await newCart.save()
+  res.json(savedCart)
+})
+
+// Update cart
+
+router.put('/:id', async (req: Request, res: Response) => {
+  const updatedCart = await Cart.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: req.body,
+    },
+    { new: true}
+  )
+  res.status(200).json(updatedCart)
+})
+
+// Update specific product in cart 
+
+router.put('/:id', async (req: Request, res: Response) => {
+  const updatedCart = await Cart.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: req.body,
+    },
+    { new: true}
+  )
+  res.status(200).json(updatedCart)
+})
+
+
+// Get user's cart
+
+router.get('/find/:userID', async (req: Request, res: Response) => {
+  const cart = await Cart.findOne({ userID: req.params.userID })
+  res.status(200).json(cart)
+})
+
+// Delete 
+router.delete('/:id', async (req: Request, res: Response) => {
+  try {
+    await Cart.findByIdAndDelete(req.params.id)
+    res.status(200).json('Cart has been deleted...')
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+
+module.exports = router;
