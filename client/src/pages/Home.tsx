2d014f6ffa876ba434bb3eb61175f9c5a01fbc87
Carlos Martinez <curlosmart@gmail.com>
2021-11-09 01:45:13 -0500

    landing page created

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,74 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
 
   return (
-    <div>Home</div>
+    <div>
+      <div className="bg-kobe-hof-shoes h-screen bg-cover flex items-center text-white font-bold">
+        <div className="ml-10 pb-10 w-1/4">
+          <div className="text-5xl mb-3">Nike Kobe 5 Protro Undefeated 'Hall of Fame'</div>
+          <Link to={`/shoe/d8efc352-2ae6-473a-b20a-70b810f388ee`} className="underline">Shop Now</Link>
+        </div>
+      </div>
+      {/* <img src="/assets/landing_page/kobes.jpeg" /> */}
+
+      <div className="px-24">
+        <div className="flex justify-between text-xl font-medium mt-4">
+          <div>Popular Brands</div>
+          <Link to={`/shoes`} className="underline">See All</Link>
+        </div>
+        <div className="flex gap-4 items-center">
+          <div className="flex-2">
+            <img src="/assets/brand_logos/nike.svg" className="w-full"/>
+          </div>
+
+          <div className="flex-2">
+            <img src="/assets/brand_logos/jordan.svg" className="w-full"/>
+          </div>
+
+          <div className="flex-2">
+            <img src="/assets/brand_logos/adidas.svg" className="w-full"/>
+          </div>
+
+          <div className="flex-2">
+            <img src="/assets/brand_logos/new balance.svg" className="w-full"/>
+          </div>
+
+          <div className="flex-2">
+            <img src="/assets/brand_logos/louis vuitton.svg" className="w-full"/>
+          </div>
+
+          <div className="flex-2">
+            <img src="/assets/brand_logos/gucci.svg" className="w-full"/>
+          </div>
+
+          <div className="flex-2">
+            <img src="/assets/brand_logos/chanel.svg" className="w-full"/>
+          </div>
+
+          
+        </div>
+
+        <div className="mt-5 flex w-full mb-10">
+          <div className="w-4/12">
+            <img src="/assets/landing_page/mens.jpeg" alt="Mens Shoe" className="w-full"/>
+            <Link to={``} className="underline font-medium text-xl">Shop Men</Link>
+          </div>
+
+          <div className="w-4/12">
+            <img src="/assets/landing_page/womens.jpeg" alt="Mens Shoe" className="w-full"/>
+            <Link to={``} className="underline font-medium text-xl">Shop Women</Link>
+          </div>
+
+          <div className="w-4/12">
+            <img src="/assets/landing_page/kids.jpeg" alt="Mens Shoe" className="w-full"/>
+            <Link to={``} className="underline font-medium text-xl">Shop Kids</Link>
+          </div>
+
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
