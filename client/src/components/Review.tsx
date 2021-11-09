eeaa985645b52394b240228eb9845da542b9557a
Carlos Martinez <curlosmart@gmail.com>
2021-11-09 10:00:56 -0500

    like and dislike reviews

--- client/src/components/Review.tsx
+++ client/src/components/Review.tsx
@@ -1,47 +1,78 @@
-import React from 'react'
-import { IRating } from '../types/types'
+import React, { useState } from 'react'
+import { IRating, UserType } from '../types/types'
 import StarRatingComponent from 'react-star-rating-component'
 import { ThumbUpIcon as ThumbUpSolid, ThumbDownIcon as ThumbDownSolid } from '@heroicons/react/outline'
 import { ThumbUpIcon as ThumbUpOutline, ThumbDownIcon as ThumbDownOutline } from '@heroicons/react/outline'
 import moment from 'moment'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '../redux/store'
+import axios from 'axios'
+import { updateUser } from '../redux/userRedux'
 
 interface Props {
   shoeRating: IRating
 }
 
 const Review = ({ shoeRating }: Props) => {
+  
+  const user: Partial<UserType> = useSelector((state: RootState) => state.user.currentUser)
+  const dispatch = useDispatch()
+  const [review, setReview] = useState(shoeRating)
 
-  console.log(shoeRating)
+  const handleLike = async () => {
+    const body = {
+      ratingID: review._id,
+      userID: user._id
+    }
+    const response = await axios.put(`http://localhost:8888/rating/like`, body)
+    console.log(response)
+    setReview({...review, helpful: response.data.updatedRating.helpful})
+    dispatch(updateUser(response.data.updatedUser))
+  }
+
+  const handleDislike = async () => {
+    const body = {
+      ratingID: review._id,
+      userID: user._id
+    }
+    const response = await axios.put(`http://localhost:8888/rating/dislike`, body)
+    console.log(response)
+    setReview({...review, notHelpful: response.data.updatedRating.notHelpful})
+    dispatch(updateUser(response.data.updatedUser))
+  }
+
+  console.log(user)
+  
   return (
     <div className="mb-6">
       <div className="flex gap-2 items-center">
-        <img src="https://images-na.ssl-images-amazon.com/images/S/amazon-avatars-global/default._CR0,0,1024,1024_SX460_.png" alt={shoeRating.postedByUser.firstName} className="h-8 w-8"/>
+        <img src="https://images-na.ssl-images-amazon.com/images/S/amazon-avatars-global/default._CR0,0,1024,1024_SX460_.png" alt={review.postedByUser.firstName} className="h-8 w-8"/>
 
-        <div className="text-sm">{shoeRating.postedByUser.firstName} {shoeRating.postedByUser.lastName}</div>
+        <div className="text-sm">{review.postedByUser.firstName} {review.postedByUser.lastName}</div>
       </div>
       <div className="flex">
         <StarRatingComponent
           name={'Rating'}
-          value={shoeRating.ratingNum}
+          value={review.ratingNum}
           starCount={5}
           editing={false}
           starColor={'#F5B327'}
         />
-        <div className="ml-2 font-bold">{shoeRating.summary}</div>
+        <div className="ml-2 font-bold">{review.summary}</div>
       </div>
 
-      <div className="text-sm text-gray-600">Reviewed on {moment(shoeRating.createdAt).format('MMMM Do, YYYY')}</div>
+      <div className="text-sm text-gray-600">Reviewed on {moment(review.createdAt).format('MMMM Do, YYYY')}</div>
       <div className="text-sm font-medium text-orange-700">Verified Purchase</div>
-      <div className="text-sm my-2">{shoeRating.text}</div>
+      <div className="text-sm my-2">{review.text}</div>
       <div className="text-sm flex gap-2">
         <div>Helpful? </div>
         <div className="flex">
-          <ThumbUpOutline className="h-5 w-5"/>
-          <span className="ml-1">{shoeRating.helpful.length}</span>
+          <ThumbUpOutline className="h-5 w-5 cursor-pointer" onClick={handleLike}/>
+          <span className="ml-1">{review.helpful.length}</span>
         </div>
         <div className="flex">
-          <ThumbUpOutline className="h-5 w-5"/>
-          <span className="ml-1">{shoeRating.helpful.length}</span>
+          <ThumbDownOutline className="h-5 w-5 cursor-pointer" onClick={handleDislike}/>
+          <span className="ml-1">{review.notHelpful.length}</span>
         </div>
       </div>
     </div>
--- server/routes/ratingRouter.ts
+++ server/routes/ratingRouter.ts
@@ -33,24 +33,54 @@ router.post('/rate', async (req: Request, res: Response) => {
   }
 })
 
-// router.put('/helpful', async (req: Request, res: Response) => {
-//   const shoe = await Shoe.findOne({shoeID: req.body.shoeID})
-//   const user = await User.findOne({_id: req.body.userID})
+router.put('/like', async (req: Request, res: Response) => {
+  
+  const rating = await Rating.findOne({_id: req.body.ratingID})
+  const user = await User.findOne({_id: req.body.userID})
 
-//   if (!shoe.favorites.includes(req.body.userID)) {
-//     await shoe.updateOne({ $push: { favorites: user._id } })
-//     await user.updateOne({ $push: { favorites: shoe._id } })
-//     const updatedShoe = await Shoe.findById(shoe._id)
-//     const updatedUser = await User.findById(user._id)
-//     res.status(200).json({updatedShoe, updatedUser})
-//   } else {
-//     await shoe.updateOne({ $pull: { favorites: user._id } })
-//     await user.updateOne({ $pull: { favorites: shoe._id } })
-//     const updatedShoe = await Shoe.findById(shoe._id)
-//     const updatedUser = await User.findById(user._id)
-//     res.status(200).json({updatedShoe, updatedUser})
-//   }
-//   res.json(shoe)
-// })
+  try {
+    if (!rating.helpful.includes(req.body.userID)) {
+      await rating.updateOne({ $push: { helpful: user._id } })
+      await user.updateOne({ $push: { helpful: rating._id } })
+      const updatedRating = await Rating.findById(rating._id)
+      const updatedUser = await User.findById(user._id)
+      res.status(200).json({updatedRating, updatedUser})
+    } else {
+      await rating.updateOne({ $pull: { helpful: user._id } })
+      await user.updateOne({ $pull: { helpful: rating._id } })
+      const updatedRating = await Rating.findById(rating._id)
+      const updatedUser = await User.findById(user._id)
+      res.status(200).json({updatedRating, updatedUser})
+    }
+  } catch (err) {
+    res.json(err)
+  }
+  res.json(rating)
+})
+
+router.put('/dislike', async (req: Request, res: Response) => {
+  
+  const rating = await Rating.findOne({_id: req.body.ratingID})
+  const user = await User.findOne({_id: req.body.userID})
+
+  try {
+    if (!rating.notHelpful.includes(req.body.userID)) {
+      await rating.updateOne({ $push: { notHelpful: user._id } })
+      await user.updateOne({ $push: { notHelpful: rating._id } })
+      const updatedRating = await Rating.findById(rating._id)
+      const updatedUser = await User.findById(user._id)
+      res.status(200).json({updatedRating, updatedUser})
+    } else {
+      await rating.updateOne({ $pull: { notHelpful: user._id } })
+      await user.updateOne({ $pull: { notHelpful: rating._id } })
+      const updatedRating = await Rating.findById(rating._id)
+      const updatedUser = await User.findById(user._id)
+      res.status(200).json({updatedRating, updatedUser})
+    }
+  } catch (err) {
+    res.json(err)
+  }
+  res.json(rating)
+})
 
 module.exports = router;
