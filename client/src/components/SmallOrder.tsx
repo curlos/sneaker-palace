87806568cdecc799191ffe3d76cf61eadbec7c6f
Carlos Martinez <curlosmart@gmail.com>
2021-11-15 14:19:01 -0500

    Small order added

--- client/src/components/SmallOrder.tsx
+++ client/src/components/SmallOrder.tsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import axios from 'axios'
+import moment from 'moment'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { IOrder, IProduct, Shoe } from '../types/types'
+import SmallOrderShoe from './SmallOrderShoe'
 
-const SmallOrder = () => {
+interface Props {
+  order: IOrder
+}
+
+const SmallOrder = ({ order }: Props) => {
+
+  const [shoes, setShoes] = useState<Array<Shoe>>()
+
+  useEffect(() => {
+    const fetchFromAPI = async () => {
+      const newShoes = await getAllShoes(order.products)
+      setShoes(newShoes)
+    }
+    fetchFromAPI()
+  }, [])
+
+  const getAllShoes = async (products: Array<IProduct>) => {
+    const newShoes = []
+    for (let product of products) {
+      const { productID } = product
+      const response = await axios.get(`http://localhost:8888/shoes/${productID}`)
+      newShoes.push(response.data)
+    }
+    return newShoes
+  }
+
+  console.log(shoes)
+
+  console.log(order)
+
   return (
-    <div>
-      small
+    <div className="border-0 border-b border-solid border-gray-400 py-5 text-gray-800">
+      <div className="flex justify-between mb-4 text-sm">
+        <div className="flex gap-10">
+          <div>
+            <div>ORDER PLACED</div>
+            <div>{moment(order.orderDate).format('MMMM Do YYYY')}</div>
+          </div>
+
+          <div>
+            <div>TOTAL</div>
+            <div>${order.amount}.00</div>
+          </div>
+        </div>
+
+        <div>
+          <div>ORDER #{order._id}</div>
+          <Link to={`/order-details/${order._id}`} className="text-blue-400 hover:underline">View order details</Link>
+        </div>
+      </div>
+
+      {shoes?.map((shoe) => <SmallOrderShoe shoe={shoe} />)}
     </div>
   )
 }
---
+++ client/src/components/SmallOrderShoe.tsx
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Shoe } from '../types/types'
+
+interface Props {
+  shoe: Partial<Shoe>
+}
+
+const SmallOrder = ({ shoe }: Props) => {
+
+  return (
+    <div className="flex">
+      <div>
+        <img src={shoe?.image?.original} className="h-150" />
+      </div>
+
+      <div>
+        <Link to={`/shoe/${shoe.shoeID}`} className="text-blue-400 hover:underline">{shoe.name}</Link>
+        <div className="text-xs">
+          <div className="text-red-800">${shoe.retailPrice}.00</div>
+          <div>{shoe.colorway}</div>
+          <div>{shoe.gender}</div>
+          <div>{shoe.sku}</div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default SmallOrder
