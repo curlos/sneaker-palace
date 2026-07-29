9ddc899c79f3e2c11db03169528eff29481b22c7
Carlos Martinez <curlosmart@gmail.com>
2026-07-29 18:08:59 -0400

    Add scaffolding for shoeRouter.filters tests

---
+++ server/routes/shoeRouter.filters.test.ts
@@ -0,0 +1,34 @@
+// import request from 'supertest';
+// import { MongoMemoryServer } from 'mongodb-memory-server';
+// import { startTestServer, stopTestServer } from '../test/testServer';
+
+// let mongod: MongoMemoryServer;
+// let app: Awaited<ReturnType<typeof startTestServer>>['app'];
+
+// beforeAll(async () => {
+// 	({ app, mongod } = await startTestServer());
+// });
+
+// afterAll(async () => {
+// 	await stopTestServer(mongod);
+// });
+
+describe('POST /shoes - sorting', () => {
+	it.todo('TODO');
+});
+
+describe('POST /shoes - filtering', () => {
+	it.todo('TODO');
+});
+
+describe('POST /shoes - search', () => {
+	it.todo('TODO');
+});
+
+describe('POST /shoes - pagination', () => {
+	it.todo('TODO');
+});
+
+describe('POST /shoes - error handling', () => {
+	it.todo('TODO');
+});
